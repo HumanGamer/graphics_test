e853ddccc138ce8b9787d69ebd0819273fed4bc0
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-04-10 22:08:54 -0500

    Setup BGFX

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <SDL.h>
+#include <bx/thread.h>
+#include <bgfx/bgfx.h>
+
+#include "sdl_bgfx_setup.h"
+
+void render();
+
+static int32_t renderThread(bx::Thread* _thread, void* _userData);
 
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
         std::cout << "Error initializing SDL: %s\n", SDL_GetError();
 
+    uint32_t width = 800, height = 600;
+
     SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED,
                                                SDL_WINDOWPOS_CENTERED,
-                                               800, 600, 0);
+                                       width, height,
+                                       SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    bgfx::renderFrame();
+    sdlSetWindow(win);
+
+    bgfx::Init init;
+    init.resolution.width = width;
+    init.resolution.height = height;
+    init.resolution.reset = BGFX_RESET_VSYNC;
+    if (!bgfx::init(init))
+        return 1;
 
     bool running = true;
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR);
+    bgfx::setViewRect(0, 0, 0, bgfx::BackbufferRatio::Equal);
     while (running)
     {
         SDL_Event event;
@@ -19,12 +43,55 @@ int main(int argc, char* argv[])
         {
             switch (event.type)
             {
+                case SDL_WINDOWEVENT:
+                {
+                    const SDL_WindowEvent &wev = event.window;
+                    switch (wev.event)
+                    {
+                        case SDL_WINDOWEVENT_RESIZED:
+                        case SDL_WINDOWEVENT_SIZE_CHANGED:
+                        {
+                            uint32_t oldWidth = width;
+                            uint32_t oldHeight = height;
+
+                            width = wev.data1;
+                            height = wev.data2;
+
+                            if (width != oldWidth || height != oldHeight)
+                            {
+                                bgfx::reset(width, height, BGFX_RESET_VSYNC);
+                                bgfx::setViewRect(0, 0, 0, bgfx::BackbufferRatio::Equal);
+                            }
+
+
+                            break;
+                        }
+                    }
+                    break;
+                }
                 case SDL_QUIT:
                     running = false;
                     break;
             }
         }
+
+        render();
     }
 
+    //bgfx::shutdown(); // sometimes crashes on quit
+    sdlDestroyWindow(win);
+    SDL_Quit();
+
     return 0;
+}
+
+void render()
+{
+    bgfx::touch(0);
+    bgfx::dbgTextClear();
+    bgfx::dbgTextPrintf(0, 0, 0x0f, "Test");
+
+    bgfx::setDebug(BGFX_DEBUG_TEXT);
+
+    bgfx::frame();
 }
---
+++ src/sdl_bgfx_setup.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <SDL_syswm.h>
+#include <bgfx/platform.h>
+
+inline static void* sdlNativeWindowHandle(SDL_Window* _window)
+{
+    SDL_SysWMinfo wmi;
+    SDL_VERSION(&wmi.version);
+    if (!SDL_GetWindowWMInfo(_window, &wmi) )
+    {
+        return NULL;
+    }
+
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+    #		if ENTRY_CONFIG_USE_WAYLAND
+		wl_egl_window *win_impl = (wl_egl_window*)SDL_GetWindowData(_window, "wl_egl_window");
+		if(!win_impl)
+		{
+			int width, height;
+			SDL_GetWindowSize(_window, &width, &height);
+			struct wl_surface* surface = wmi.info.wl.surface;
+			if(!surface)
+				return nullptr;
+			win_impl = wl_egl_window_create(surface, width, height);
+			SDL_SetWindowData(_window, "wl_egl_window", win_impl);
+		}
+		return (void*)(uintptr_t)win_impl;
+#		else
+		return (void*)wmi.info.x11.window;
+#		endif
+#	elif BX_PLATFORM_OSX
+    return wmi.info.cocoa.window;
+#	elif BX_PLATFORM_WINDOWS
+    return wmi.info.win.window;
+#	endif // BX_PLATFORM_
+}
+
+inline bool sdlSetWindow(SDL_Window* _window)
+{
+    SDL_SysWMinfo wmi;
+    SDL_VERSION(&wmi.version);
+    if (!SDL_GetWindowWMInfo(_window, &wmi) )
+    {
+        return false;
+    }
+
+    bgfx::PlatformData pd;
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+    #		if ENTRY_CONFIG_USE_WAYLAND
+		pd.ndt          = wmi.info.wl.display;
+#		else
+		pd.ndt          = wmi.info.x11.display;
+#		endif
+#	elif BX_PLATFORM_OSX
+    pd.ndt          = NULL;
+#	elif BX_PLATFORM_WINDOWS
+    pd.ndt          = NULL;
+#	endif // BX_PLATFORM_
+    pd.nwh          = sdlNativeWindowHandle(_window);
+
+    pd.context      = NULL;
+    pd.backBuffer   = NULL;
+    pd.backBufferDS = NULL;
+    bgfx::setPlatformData(pd);
+
+    return true;
+}
+
+inline static void sdlDestroyWindow(SDL_Window* _window)
+{
+    if(!_window)
+        return;
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+    #		if ENTRY_CONFIG_USE_WAYLAND
+		wl_egl_window *win_impl = (wl_egl_window*)SDL_GetWindowData(_window, "wl_egl_window");
+		if(win_impl)
+		{
+			SDL_SetWindowData(_window, "wl_egl_window", nullptr);
+			wl_egl_window_destroy(win_impl);
+		}
+#		endif
+#	endif
+    SDL_DestroyWindow(_window);
+}
