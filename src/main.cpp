25cd71042f67d09f3a0da699861e9a917766e6cc
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-04-11 15:55:03 -0500

    Separate init function

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 
 #include "graphics/GraphicsSystem.hpp"
 
+void init();
 void gameLoop();
 void render();
 void cleanup();
 
 int main(int argc, char *argv[])
 {
-    GFX->init(800, 600);
+    init();
 
     gameLoop();
 
@@ -23,11 +24,16 @@ void cleanup()
     delete GFX;
 }
 
-void gameLoop()
+void init()
 {
+    GFX->init(800, 600);
+
     bgfx::setViewClear(0, BGFX_CLEAR_COLOR);
     bgfx::setViewRect(0, 0, 0, bgfx::BackbufferRatio::Equal);
+}
 
+void gameLoop()
+{
     while (true)
     {
         render();
