26f76776ae95e2d2be6a0c261cc170f5c7fed0ec
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-04-11 20:29:35 -0500

    Updated bgfx.cmake

---
+++ src/shaders/Shader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mattp on 4/11/2021.
+//
+
+#include "Shader.hpp"
---
+++ src/shaders/Shader.hpp
@@ -0,0 +1,15 @@
+//
+// Created by mattp on 4/11/2021.
+//
+
+#ifndef GRAPHICS_TEST_SHADER_HPP
+#define GRAPHICS_TEST_SHADER_HPP
+
+
+class Shader
+{
+
+};
+
+
+#endif //GRAPHICS_TEST_SHADER_HPP
